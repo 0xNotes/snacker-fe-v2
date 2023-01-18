3bfc826428c103a4c007375da14d33fa5e986f05
0xNotes <notes@sigil.systems>
2023-01-17 23:35:51 -0500

    added rainbowkit shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import "./styles/styles.css"
+import HomePage from './pages/HomePage';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    //alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
 
 function App() {
   return (
-    <div>
-      Test
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <HomePage />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
