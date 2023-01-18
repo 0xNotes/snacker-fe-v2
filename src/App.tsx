d3181baf61439dee9a11712d814dbe77b515d3a4
0xNotes <notes@sigil.systems>
2023-01-17 23:15:13 -0500

    Fixed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import "./styles/styles.css"
+
+function App() {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
+
+export default App;
